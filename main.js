a227eba14ee16d4106d9eba52630cf4fd2cabbc3
krzysztof <krzysztof.rozga@student.put.poznan.pl>
2017-06-01 22:31:19 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+/**
+ * Created by krzysztof on 01/06/2017.
+ */
+"use strict";
+var base = "http://localhost:8000/";
+
+
+var operations = function(url) {
+    var self = ko.observableArray();
+
+    self.get = function() {
+        $.ajax({
+            url: url,
+            success: function (data) {
+
+                $.each(data, function (i, item) {
+                    console.log(item);
+                    self.push(item);
+                })
+            }
+        })
+    }
+
+    return self;
+}
+
+
+function ViewModel() {
+    var self = this;
+
+    self.courses = new operations(base + "courses");
+    self.courses.get();
+
+    console.log(self.courses);
+}
+
+
+
+var viewModel = new ViewModel();
+
+$(document).ready(function () {
+    ko.applyBindings(viewModel);
+});
+
