ae28b0ad7a0b48445fbe4e0572839704103ac6a6
krzysztof <krzysztof.rozga@student.put.poznan.pl>
2017-06-08 19:34:18 +0200

    Student and course can be modified, added or deleted.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * Created by krzysztof on 01/06/2017.
  */
 "use strict";
-var base = "http://localhost:8000/";
+var base = "http://localhost:8000";
 
 
 var operations = function(url) {
@@ -12,29 +12,106 @@ var operations = function(url) {
         $.ajax({
             url: url,
             success: function (data) {
-
                 $.each(data, function (i, item) {
-                    console.log(item);
                     self.push(item);
                 })
             }
         })
+    };
+
+    self.post = function(object) {
+        console.log("Started POST");
+        console.log(object);
+        $.ajax({
+            method: "POST",
+            url: url,
+            contentType: "application/json",
+            data: ko.mapping.toJSON(object),
+            success: function() {
+                //alert("Dodano pomyślnie");
+                self.removeAll();
+                self.get();
+            },
+            error: function () {
+                alert("Nie dodano!");
+            }
+        })
+    };
+
+    self.put = function (object) {
+        console.log(object);
+        $.ajax({
+            method: "PUT",
+            url: base + links(object).self,
+            contentType: "application/json",
+            data: ko.mapping.toJSON(object),
+            success: function () {
+                self.removeAll();
+                self.get();
+            },
+            error: function () {
+                alert("Nie uaktualniono!");
+            }
+        })
+    }
+
+    self.delete = function(object) {
+        console.log(object);
+        $.ajax({
+            method: "DELETE",
+            url: base + links(object).self,
+            success: function () {
+                //alert("Usunięto pomyślnie!");
+                self.removeAll();
+                self.get();
+            },
+            error: function () {
+                console.log(object);
+                alert("Błąd usuwania!");
+            }
+        })
+    }
+
+
+    function links(object) {
+        return object.link.reduce(function(map, obj) {
+            map[obj.params.rel] = obj.href;
+            return map;
+        }, {});
     }
 
     return self;
-}
+};
 
 
 function ViewModel() {
     var self = this;
 
-    self.courses = new operations(base + "courses");
-    self.courses.get();
+    self.indexInput = ko.observable();
+    self.nameInput = ko.observable();
+    self.surnameInput = ko.observable();
+    self.birthdateInput = ko.observable();
 
-    self.students = new operations(base + "students");
+    self.students = new operations(base + "/students");
     self.students.get();
+    self.students.new = function () {
+        self.students.post({index: self.indexInput, name: self.nameInput, surname: self.surnameInput, birthdate: self.birthdateInput});
+    };
 
-    console.log(self.courses);
+
+
+    self.courseName = ko.observable();
+    self.lecturerName = ko.observable();
+
+    self.courses = new operations(base + "/courses");
+    self.courses.get();
+    self.courses.new = function () {
+        self.courses.post({name: self.courseName, lecturer: self.lecturerName});
+    };
+    self.courses.seeGrades = function () {
+        document.getElementById("courseGrades").style.display = "block";
+        document.getElementById("coursesList").style.display = "none";
+    }
 }
 
 
